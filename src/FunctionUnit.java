577df8c3a52c7df9ddf2a9814599118bac9a0bcd
Balakrishna <bklellapalli@gmail.com>
2017-07-28 20:42:46 -0700

    Add Instructions and Parser
    Add src/FunctionUnit.java
    Add src/Instruction.java
    Add src/KeyValue.java
    Add src/Parser.java

--- src/FunctionUnit.java
+++ src/FunctionUnit.java
@@ -5,72 +5,61 @@
 public class FunctionUnit {
 
 	//In EX stage:  Perform Arithmetic operation on input operands and store in destination field of Instruction Object
-	public Instruction executeInstruction(Instruction instruction){
+	public static Instruction executeInstruction(Instruction instruction) {
 
-		switch(instruction.getOperation()){
+		switch (instruction.getOperation()) {
 
-		//Register to Register Instruction (Source1 and Source2)
-		case Operations.ADD: 
-			instruction.setDestination(instruction.getSrc1().getValue() +  instruction.getSrc2().getValue());
+		// Register to Register Instruction
+		case Consts.ADD:
+			instruction.setDestination(instruction.getSrc1().getValue()
+					+ instruction.getSrc2().getValue());
 			break;
-		case Operations.SUB:
-			instruction.setDestination(instruction.getSrc1().getValue() - instruction.getSrc2().getValue());
+		case Consts.SUB:
+			instruction.setDestination(instruction.getSrc1().getValue()
+					- instruction.getSrc2().getValue());
 			break;
-		case Operations.MUL:
-			instruction.setDestination(instruction.getSrc1().getValue() * instruction.getSrc2().getValue());
+		case Consts.MUL:
+			instruction.setDestination(instruction.getSrc1().getValue()
+					* instruction.getSrc2().getValue());
 			break;
-		case Operations.AND:
-			instruction.setDestination(instruction.getSrc1().getValue() & instruction.getSrc2().getValue());
+		case Consts.AND:
+			instruction.setDestination(instruction.getSrc1().getValue()
+					& instruction.getSrc2().getValue());
 			break;
-		case Operations.OR:
-			instruction.setDestination(instruction.getSrc1().getValue() | instruction.getSrc2().getValue());
+		case Consts.OR:
+			instruction.setDestination(instruction.getSrc1().getValue()
+					| instruction.getSrc2().getValue());
 			break;
-		case Operations.EXOR:
-			instruction.setDestination(instruction.getSrc1().getValue() ^ instruction.getSrc2().getValue());
+		case Consts.EXOR:
+			instruction.setDestination(instruction.getSrc1().getValue()
+					^ instruction.getSrc2().getValue());
 			break;
 		//Move literal/ Source value into destination field 
-		case Operations.MOV:
-		case Operations.MOVC:
-			if(instruction.getSrc1() != null)
+		case Consts.MOV:
+		case Consts.MOVC:
+			if (instruction.getSrc1() != null)
 				instruction.setDestination(instruction.getSrc1().getValue());
 			else
 				instruction.setDestination(instruction.getLiteral());
 			break;
-
 		//Memory Instruction(Load from memory/ Store in memory)
-		case Operations.LOAD:
-		case Operations.STORE:
-			if(instruction.getSrc2() != null)
-				instruction.setMemoryAddress(instruction.getSrc1().getValue() +  instruction.getSrc2().getValue());
-			else 
-				instruction.setMemoryAddress(instruction.getSrc1().getValue() +  instruction.getLiteral());
+		case Consts.LOAD:
+		case Consts.STORE:
+			if (instruction.getSrc2() != null)
+				instruction.setMemoryAddress(instruction.getSrc1().getValue()
+						+ instruction.getSrc2().getValue());
+			else
+				instruction.setMemoryAddress(instruction.getSrc1().getValue()
+						+ instruction.getLiteral());
+			break;
+		//Control Flow Instruction(unconditional jump instruction)	
+		case Consts.JUMP:
+			instruction.setSrc1(instruction.getSrc1().getValue() + 
+					instruction.getLiteral());
+			break;
+		default:
 			break;
 		}
 		return instruction;
-	}
-
-	public Integer predictBranch(Instruction instruction, Integer pDestination, Integer currentPC, Integer registerValue, Integer specialRegister){
-		switch(instruction.getOperation()){
-		//Control Flow Instruction(Conditional/ unconditional jump instruction)	
-		case Operations.BNZ:
-			if(pDestination != 0)
-				currentPC = currentPC + instruction.getLiteral() - 2; //Relative address
-			break;
-		case Operations.BZ:
-			if(pDestination == 0)
-				currentPC = currentPC + instruction.getLiteral() - 2; //Relative address
-			break;
-		case Operations.JUMP:
-			// X is Special register used to store next PC address in case of BAL instruction
-			if(!instruction.getDestination().getKey().equals("X"))
-				currentPC =  registerValue + instruction.getLiteral() - 2;
-			else
-				currentPC = specialRegister + instruction.getLiteral();
-			break;
-		case Operations.BAL:
-			currentPC = registerValue +  instruction.getLiteral() - 2;	
-			break;
-		}
-		return currentPC;
 	}
 }
--- src/KeyValue.java
+++ src/KeyValue.java
@@ -3,30 +3,31 @@
  *
  */
 import java.util.Map;
+
 final class KeyValue<K, V> implements Map.Entry<K, V> {
-	
-    private final K key;
-    private V value;
 
-    public KeyValue(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
+	private final K key;
+	private V value;
 
-    @Override
-    public K getKey() {
-        return key;
-    }
+	public KeyValue(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
 
-    @Override
-    public V getValue() {
-        return value;
-    }
+	@Override
+	public K getKey() {
+		return key;
+	}
 
-    @Override
-    public V setValue(V value) {
-        V old = this.value;
-        this.value = value;
-        return old;
-    }
+	@Override
+	public V getValue() {
+		return value;
+	}
+
+	@Override
+	public V setValue(V value) {
+		V old = this.value;
+		this.value = value;
+		return old;
+	}
 }
